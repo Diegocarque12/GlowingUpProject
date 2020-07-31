4a4069c001be07ee5805c6e1a4809754c809938c
Keilyn Ramirez <keilyn148@gmail.com>
2020-07-30 22:18:35 -0600

    cols

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="row">
-        <div class="col-md-6 col-lg-6 col-xl-6">
+        <div class="col-auto col-sm-12 col-md-6 col-lg-6 col-xl-6">
         </div>
     </div>
 </footer>
