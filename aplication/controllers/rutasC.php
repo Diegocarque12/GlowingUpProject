03202ca3b53067813cdafc23dfdbcc5577076933
Diego Carvajal <diegocarque@hotmail.com>
2020-08-03 14:30:49 -0600

    layouts

--- aplication/controllers/rutasC.php
+++ aplication/controllers/rutasC.php
@@ -3,21 +3,21 @@
     class RutasControlador{
 
         public function Plantilla(){
-            include "views/layout/plantilla.php";
-        }
-    }
-
-    public function Rutas(){
-        if(isset($_GET["ruta"])){
-            $rutas = $_GET["ruta"];
-        }else{
-            $rutas = "index";
+            include "../aplication/views/plantilla.php";
         }
 
-        $respuesta = Modelo::RutasModelo($rutas);
-
-        include $respuesta;
+        public function Rutas(){
+            if(isset($_GET["ruta"])){
+                $rutas = $_GET["ruta"];
+            }else{
+                $rutas = "index";
+            }
+    
+            $respuesta = Modelo::RutasModelo($rutas);
+    
+            include $respuesta;
+    
+        }
 
     }
-
 ?>
