58fe0a19cd2c016e9f674fd350d7f8088774fa7e
Diego Carvajal <diegocarque@hotmail.com>
2020-08-05 17:12:53 -0600

    conexion completa

--- db/Producto.php
+++ db/Producto.php
@@ -1,6 +1,5 @@
 <?php
 require_once("autoload.php");
-require_once("Conexion.php");
 
 class Producto extends Conexion{
     private $db_idProducto;
@@ -15,14 +14,18 @@ class Producto extends Conexion{
     private $db_descuento;
     private $db_valoracion_general;
     private $db_idMarca;
-
     private $conexion;
 
-    public function __contruct(){
+    public function __construct(){
+        /*$this->conexion = new Conexion();
+        echo "paso1";
+        $this->conexion = $this->conexion->connect();
+        echo "paso2";*/
+        //echo "paso2".$this->conexion->connect();
+        include_once('Conexion.php');
         $this->conexion = new Conexion();
-        /*include_once('Conexion.php');
-        $conexion = new Conexion();
-        $conexion->__construct();*/
+        echo 'lista la nueva conexion   /   ';
+        /*$conexion->__construct();*/
     }
 
     public function insertProducto(int $idProducto, string $nombre, string $imagenes, string $descripcion_extensa, string $descripcion_breve, float $precioVenta, int $cantidad, string $colores, string $estado, float $descuento, float $valoracion_general, int $idMarca){
@@ -39,12 +42,15 @@ class Producto extends Conexion{
         $this->db_valoracion_general = $valoracion_general;
         $this->db_idMarca = $idMarca;
 
+        /*$this->conexion = new Conexion();
+        $this->conexion = $this->conexion->connect();*/
+
         $sql = "INSERT INTO tblproducto(idProducto, nombre, imagenes, descripcion_extensa, descripcion_breve, precioVenta, cantidad, colores, estado, descuento, valoracion_general, idMarca) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
         $insert = $this->conexion->prepare($sql);
         $arrData = array($this->db_idProducto,$this->db_nombre,$this->db_imagenes,$this->db_descripcion_extensa,$this->db_descripcion_breve,$this->db_precioVenta,$this->db_cantidad,$this->db_colores,$this->db_estado,$this->db_descuento,$this->db_valoracion_general,$this->db_idMarca);
         $resInsert = $insert->execute($arrData);
         $idInsert = $this->conexion->lastInsertId();
-        return $idInsert;
+        echo $idInsert;
     }
 
 }
